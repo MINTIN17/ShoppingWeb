92b19f78e23d7fcc032f4bced66cd19371729e35
phuchuy <phuchuy1212003@gmail.com>
2025-11-01 23:51:34 +0700

    huy's second commit

--- ShoppingWebApi/src/main/java/com/example/ShoppingWebApi/domain/entities/User.java
+++ ShoppingWebApi/src/main/java/com/example/ShoppingWebApi/domain/entities/User.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-// Huy arrived
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
